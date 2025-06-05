65d44c606d09814e8c4e3e53bf722e11373a03e6
Cesar Gattano <cesar.gattano@gmail.com>
2025-06-05 13:04:04 +0200

    feat: :sparkles: Model room created

--- app/models/room.py
+++ app/models/room.py
@@ -1,18 +1,27 @@
-from sqlmodel import Field, SQLModel, Column, JSON
+from sqlmodel import Field, SQLModel, create_engine
 
 
 class Room(SQLModel, table=True):
+    """
+    Room model to describe each room in the training center.
+    """
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
     capacity: int = Field(ge=1)
-    location: str
-    equipments: dict = Field(default=None, sa_column=Column(JSON))
+    location: str = Field(schema_extra={"example": "Building North, 1st floor"})
     is_active: bool = Field(default=True)
 
 
 def test():
-    room1 = Room(name="A101", capacity=12, location="Building North, 1st floor", equipments={"beamer": 1, "white board": 1})
+    room1 = Room(name="A101", capacity=12, location="Building North, 1st floor")
     print(room1)
+
+    sqlite_file_name = "database.db"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+    engine = create_engine(sqlite_url, echo=True)
+
+    SQLModel.metadata.create_all(engine)
 
 if __name__ == "__main__":
     test()
