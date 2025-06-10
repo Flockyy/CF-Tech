c6fc61810b85b5d7c700c3d3dad357121fafdae5
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-06-10 14:05:41 +0200

    refactor: :lock: Adding .env file (not to github)

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,15 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -38,7 +44,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option(os.getenv("DATABASE_URL"))
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -64,9 +71,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,6 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
 
 
 class Settings(BaseSettings):
@@ -6,11 +8,14 @@ class Settings(BaseSettings):
     Application settings loaded from environment variables or a .env file.
     """
 
+    load_dotenv()
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    DB_ADMIN_EMAIL = os.getenv("DB_ADMIN_EMAIL")
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///./app.db"
-
-    FIRST_SUPERUSER: str = "admin@example.com"
+    SQLALCHEMY_DATABASE_URI: str = DATABASE_URL
+    FIRST_SUPERUSER: str = DB_ADMIN_EMAIL
 
 
 settings = Settings()  # type: ignore
