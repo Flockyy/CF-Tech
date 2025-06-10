952423562c139e102973422985d639af6b815031
Cesar Gattano <cesar.gattano@gmail.com>
2025-06-10 10:35:47 +0200

    feat: :sparkles: Create model for link table between room and equipment

--- app/models/room.py
+++ app/models/room.py
@@ -2,6 +2,20 @@ import uuid
 
 from sqlmodel import Field, SQLModel, Relationship
 
+
+class RoomEquipmentLink(SQLModel, table=True):
+    """
+    Model for linking a room and an equipment (they are described below).
+    The rules applied here are directly the rule applied to the SQL database.
+    """
+
+    room_id: uuid.UUID = Field(default=None, foreign_key="rooms.id", primary_key=True)
+    equipment_id: uuid.UUID = Field(
+        default=None, foreign_key="equipments.id", primary_key=True
+    )
+    quantity: int = Field(default=1, ge=1)
+
+
 class RoomBase(SQLModel, table=True):
     """
     Room model to describe each entry of room in the database.
@@ -16,7 +30,9 @@ class RoomBase(SQLModel, table=True):
     capacity: int | None = Field(default=None, ge=1)
     is_active: bool = Field(default=True)
 
-    equipments: list["EquipmentBase"] = Relationship(back_populates="room")
+    equipments: list["EquipmentBase"] = Relationship(
+        back_populates="rooms", link_model=RoomEquipmentLink
+    )
 
 
 class EquipmentBase(SQLModel, table=True):
@@ -30,20 +46,31 @@ class EquipmentBase(SQLModel, table=True):
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(..., unique=True, min_length=2, max_length=50)
-    serial_number: str | None = Field(default=None, unique=True, min_length=2, max_length=50)
+    serial_number: str | None = Field(
+        default=None, unique=True, min_length=2, max_length=50
+    )
 
-    id_room: uuid.UUID | None = Field(default=None, foreign_key="rooms.id")
-    room: RoomBase | None = Relationship(back_populates="equipments")
-
+    rooms: list[RoomBase] = Relationship(
+        back_populates="equipments", link_model=RoomEquipmentLink
+    )
 
 
 def test():
-    room1 = RoomBase(name="A101", capacity=12, location="Building North, 1st floor")
+    room1 = RoomBase(name="A101", location="Building North, 1st floor", capacity=12)
     print(room1)
     equipment1 = EquipmentBase(name="Welcome desk")
     print(equipment1)
-    equipment2 = EquipmentBase(name="White board", id_room=room1.id)
+    equipment2 = EquipmentBase(name="TV", serial_number="fraetrae42064a1et6re4t")
     print(equipment2)
+    link1 = RoomEquipmentLink(
+        room_id=uuid.uuid4(), equipment_id=uuid.uuid4(), quantity=3
+    )
+    print(link1)
+
+    room2 = RoomBase(name="A102", location="Building North, 1st floor", capacity=36, equipments=[equipment1, equipment2])
+    print(room2)
+    equipment3 = EquipmentBase(name="White board", rooms=[room1])
+    print(equipment3)
 
 
 if __name__ == "__main__":
